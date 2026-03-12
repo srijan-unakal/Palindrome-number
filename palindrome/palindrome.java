9927b5036c043dfb9fe8ffe903a170c6913fc755
srijan unakal <srijanunakal777@gmail.com>
2026-03-13 00:06:17 +0530

    Program to check palindrome number

---
+++ palindrome/palindrome.java
@@ -0,0 +1,41 @@
+package palindrome ;
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+	System.out.println("This program checks whether a given number is palindrome ");
+	while(true){
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        int original = num;
+        int rev = 0;
+
+        while (num != 0) {
+
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+
+        }
+
+        if (original == rev) {
+            System.out.println("Palindrome number");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+	System.out.println("Do you want to continue (Y/N): ");
+	char b=Character.toUpperCase(sc.next().charAt(0));
+	if (b=='N') {
+		System.out.println("Thank you ");
+		break;
+		}
+		
+
+	}
+	sc.close();
+
+    }
+}
